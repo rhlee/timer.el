5ee30f3da25f2d52671f2e42a9a8bd5c8a61d840
Richard H Lee <ricardohenrylee@gmail.com>
2013-12-15 20:38:02 +0000

    temp

--- timer.el
+++ timer.el
@@ -14,9 +14,37 @@
   (redraw-timers)
   (add-hook 'kill-buffer-query-functions 'timer-quit nil t))
 
+(defun new-timer (name)
+  (interactive "sname: ")
+  (setq timers (append timers (list (timer-new name)))))
+
+(defun timer-new (name)
+  (let ((timer (list
+      :name name
+      :time 0
+      :start nil)))
+    timer))
+
 (defun timer-quit()
   (print "hello")
   t)
 
+(defun timer-draw-button (timer)
+  (print (point))
+  (insert "hello\n"))
+
 (defun redraw-timers ()
-  (interactive))
+  (interactive)
+  (dolist (timer timers)
+    (timer-draw-button timer)))
+
+(timer)
+;(insert (propertize "foo" 'face '(:foreground "red")))
+;(with-temp-buffer)
+;(insert (with-temp-buffer (insert-text-button "hello" 'action (lambda () nil))
+;  (buffer-string)))
+;(setq overwrite-mode overwrite-mode-textual)
+(new-timer "hello")
+(new-timer "apple")
+(new-timer "bear")
+(redraw-timers)
